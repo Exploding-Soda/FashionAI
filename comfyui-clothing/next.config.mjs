afc74276263237d9277e75d42a087fd5401f9445
Exploding-Soda <68855137+Exploding-Soda@users.noreply.github.com>
2025-10-17 16:07:21 +0800

    build

--- comfyui-clothing/next.config.mjs
+++ comfyui-clothing/next.config.mjs
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    appDir: true,
+  },
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
